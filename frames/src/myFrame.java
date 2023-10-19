06e0b85a3484654d272881744f1f835fdcc6d506
Areeba Tariq <areebatariq885@gmail.com>
2023-10-19 09:49:10 +0500

    Modify frame

--- frames/src/myFrame.java
+++ frames/src/myFrame.java
@@ -12,6 +12,6 @@ public class myFrame extends JFrame {
 
         ImageIcon image = new ImageIcon("src//logo.PNG");
         this.setIconImage(image.getImage());
-        this.getContentPane().setBackground(new Color(123, 50, 250));
+        this.getContentPane().setBackground(new Color(123, 50, 250));///or write Color.green instead of rgb value
     }
 }
